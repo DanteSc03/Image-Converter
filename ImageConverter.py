e232ec0e67cdc66bc6929823a60291f4d05c0776
Dante Schrantz <149951163+DanteSc03@users.noreply.github.com>
2025-03-05 09:09:46 +0100

    Update ImageConverter.py

--- ImageConverter.py
+++ ImageConverter.py
@@ -1,11 +1,17 @@
 from PIL import Image
+from PIL.ExifTags import TAGS
 
 def resize_image(input_path, output_path, new_width):
     img = Image.open(input_path)
+    exif_data = img.info.get("exif")  # Retrieve EXIF data if present
+    
     aspect_ratio = img.height / img.width
     new_height = int(new_width * aspect_ratio)
     
     img_resized = img.resize((new_width, new_height), Image.LANCZOS)
-    img_resized.save(output_path, quality=100)
+    
+    # Save with EXIF data
+    img_resized.save(output_path, quality=100, exif=exif_data) if exif_data else img_resized.save(output_path, quality=100)
+
 
 resize_image("/Path/to/input.jpg", "/Path/to/output.jpg", 1280)
